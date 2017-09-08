4c85fd6a8920bc29c68d2cf466243037e2c239fd
msramalho <up201403027@fe.up.pt>
2017-09-08 23:37:24 +0100

    cá vai wrapper

---
+++ server/wrapper.js
@@ -0,0 +1,29 @@
+var User = require('./classes/user.js');
+var Code = require('./classes/code.js');
+var Bid = require('./classes/bid.js');
+
+class Wrapper {
+    //callback receives new User 
+    static receiveCallback(token, refreshToken, callback) {
+        User.insertNew(token, refreshToken, callback);
+    }
+
+    //calback receives userId
+    static validateSession(scode, callback) {
+        User.validateSession(scode, callback);
+    }
+
+    //callback receives codeId
+    static generateCode(userId, callback) {
+        Code.generateCode(userId, function(c) {
+            callback(c.codeId);
+        });
+    }
+
+    //callback receives double representing total in euros
+    static getDebt(code, callback) {
+        Bid.getDebt(code, callback);
+    }
+}
+
+module.exports = Wrapper;
