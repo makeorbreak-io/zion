34bdd15279cb38309d899c2450c79da882d01d54
Areadrill <jackthemotorcycle@gmail.com>
2017-09-08 20:29:00 +0000

    D A A B B iiii i some interface work

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
 
+  newSession(){
+    this.navCtrl.push(LoginPage);
+  }
 }
