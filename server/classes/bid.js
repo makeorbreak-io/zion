a68eb90d35856999bb3f80732463a2fb66251edd
msramalho <up201403027@fe.up.pt>
2017-09-08 23:37:51 +0100

    aí vai bid

---
+++ server/classes/bid.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbcon = require('./dbcon.js');
+
+class Bid {
+    constructor(bidId, codeId, songId, amount, timestamp, roundID) {
+        this.bidId = bidId;
+        this.codeId = codeId;
+        this.songId = songId;
+        this.amount = amount;
+        this.timestamp = timestamp;
+        this.roundID = roundID;
+    }
+
+    static insertNew(codeId, songId, amount, roundId, callback) {
+        var b = new Bid(0, codeId, codeId, amount, 0, roundId);
+        //insert into db
+        dbcon.query('INSERT INTO bids SET ?', { codeId: b.codeId, songId: b.songId, amount: b.amount }, function(err, result) {
+            if (err) throw err;
+            b.codeId = result.insertId;
+            callback(b);
+        });
+    }
+
+    static getDebt(codeId, callback) {
+        dbcon.query("SELECT amount, roundId FROM bids WHERE codeId = ?", [codeId], function(err, result, fields) {
+            if (err) throw err;
+        });
+    }
+
+}
+
+module.exports = Bid;
