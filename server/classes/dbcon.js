aaa440655a2067f6e842c4d58ffc77534553e98c
msramalho <up201403027@fe.up.pt>
2017-09-08 23:54:42 +0100

    use strict

--- server/classes/dbcon.js
+++ server/classes/dbcon.js
@@ -1,33 +1,11 @@
-//var mysql = require('mysql-wrapper');
-/*var conn = mysql({
-    host: "localhost",
-    database: "zion",
-    user: "root",
-    password: "minda_171",
-    port: 3306
-});*/
-/*var conn = mysql({
-    host: "localhost",
-    database: "zion",
-    user: "zion",
-    password: "minda_171"
-});
-
-conn.query('SELECT * FROM users {{where data}}', {
-    data: {
-        userId: 1
-    }
-}, function(err, result) {
-    console.log(err, result);
-});*/
-
 var mysql = require('mysql');
 
 var dbcon = mysql.createConnection({
     host: "localhost",
     database: "zion",
-    user: "zion",
-    password: "minda_171" //, port: 3306
+    user: "root",
+    password: "novapass_4",
+    port: 3306
 });
 
 dbcon.connect(function(err) {
@@ -40,8 +18,3 @@ module.exports.dbcon = dbcon;
 module.exports.query = function(query, data, callback) {
     dbcon.query(query, data, callback);
 };
-
-/*dbcon.query("SELECT * FROM users", function(err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-});*/
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express'); // Express web server framework
 const request = require('request'); // "Request" library
 const querystring = require('querystring');
@@ -13,14 +15,14 @@ const CLIENT_SECRET = "05438ca3a01845f59ce0f3bafdfd1f48";
 
 const app = express();
 
-router.use(function(req, res, next){
-  console.log("passei por aqui");
-  next();
+router.use(function(req, res, next) {
+    console.log("passei por aqui");
+    next();
 });
 
 
 app.use(express.static(__dirname + '/public'))
-   .use(cookieParser());
+    .use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -28,201 +30,201 @@ app.use(bodyParser.json());
 /* Session-related endpoints */
 
 router.route('/callback')
-  .get(function(req, res){
-    if(!req.query.code){
-      res.status(400);
-      res.json({'error': 'No code in request params'});
-    }
+    .get(function(req, res) {
+        if (!req.query.code) {
+            res.status(400);
+            res.json({ 'error': 'No code in request params' });
+        }
 
-    if(!req.query.state){
-      res.status(400);
-      res.json({'error': 'No state in request params'});
-    }
+        if (!req.query.state) {
+            res.status(400);
+            res.json({ 'error': 'No state in request params' });
+        }
 
-    let code = req.query.code;
+        let code = req.query.code;
 
-    request.get({'url': "http://ipecho.net/plain"}, function(error, response, body){
-      console.log(response);
-    });
+        request.get({ 'url': "http://ipecho.net/plain" }, function(error, response, body) {
+            console.log(response);
+        });
 
-    //TODO: Get tokens from spotify api
-    /*var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
+        //TODO: Get tokens from spotify api
+        /*var authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          form: {
+            code: code,
+            redirect_uri: redirect_uri,
+            grant_type: 'authorization_code'
+          },
+          headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+          },
           json: true
         };
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
+        request.post(authOptions, function(error, response, body) {
+          if (!error && response.statusCode === 200) {
 
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });*/
+            var access_token = body.access_token,
+                refresh_token = body.refresh_token;
 
-  });
+            var options = {
+              url: 'https://api.spotify.com/v1/me',
+              headers: { 'Authorization': 'Bearer ' + access_token },
+              json: true
+            };
+
+            // use the access token to access the Spotify Web API
+            request.get(options, function(error, response, body) {
+              console.log(body);
+            });
+
+            // we can also pass the token to the browser to make requests from there
+            res.redirect('/#' +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token
+              }));
+          } else {
+            res.redirect('/#' +
+              querystring.stringify({
+                error: 'invalid_token'
+              }));
+          }
+        });*/
+
+    });
 
 router.route('/validatesession')
-  .post(function(req, res){
-    if(!req.body.code){
-      res.status(400);
-      res.json({'error': 'No code in request body'});
-    }
+    .post(function(req, res) {
+        if (!req.body.code) {
+            res.status(400);
+            res.json({ 'error': 'No code in request body' });
+        }
 
-    let code = req.body.code;
+        let code = req.body.code;
 
-    let sessionId;
+        let sessionId;
 
-    Wrapper.validateSession(code, function(id) {
-      console.log("ID:" + id);
-      sessionId = id;
+        Wrapper.validateSession(code, function(id) {
+            console.log("ID:" + id);
+            sessionId = id;
+
+        });
+
+
+        res.status(200);
+        res.json({ 'sessionId': sessionId, 'code': code });
 
     });
-
-
-    res.status(200);
-    res.json({'sessionId': sessionId, 'code': code});
-
-  });
 
 router.route('/clientcode')
-  .get(function(req, res){
-    if(!req.query.sessId){
-      res.status(400);
-      res.json({'error': 'No session ID in request params'});
-    }
+    .get(function(req, res) {
+        if (!req.query.sessId) {
+            res.status(400);
+            res.json({ 'error': 'No session ID in request params' });
+        }
 
-    let sessId = req.query.sessId;
-    let code;
+        let sessId = req.query.sessId;
+        let code;
 
 
-    Wrapper.generateCode(sessId, function(newCode){
-      console.log("Code" + newCode);
-      code = newCode;
+        Wrapper.generateCode(sessId, function(newCode) {
+            console.log("Code" + newCode);
+            code = newCode;
+        });
+
+        res.status(200);
+        res.json({ 'code': code, 'sessId': sessId });
+
     });
 
-    res.status(200);
-    res.json({'code': code, 'sessId': sessId});
-
-  });
-
 router.route('/debt')
-  .get(function(req, res){
-    if(!req.query.code){
-      res.status(400);
-      res.json({'error': 'No code in request params'});
-    }
+    .get(function(req, res) {
+        if (!req.query.code) {
+            res.status(400);
+            res.json({ 'error': 'No code in request params' });
+        }
 
-    let code = res.query.code;
+        let code = res.query.code;
 
-    //TODO: get client's debt from DB
+        //TODO: get client's debt from DB
 
-    res.status(200);
-    res.json({'code': code, 'debt': 'UnDeR CoNsTrUcTiOn'});
-  });
+        res.status(200);
+        res.json({ 'code': code, 'debt': 'UnDeR CoNsTrUcTiOn' });
+    });
 
 /* Client-related endpoints */
 
 router.route('/validatecode')
-  .get(function(req, res){
-    if(!req.query.code){
-      res.status(400);
-      res.json({'error': 'No code in request params'});
-    }
+    .get(function(req, res) {
+        if (!req.query.code) {
+            res.status(400);
+            res.json({ 'error': 'No code in request params' });
+        }
 
-    let code = res.query.code;
+        let code = res.query.code;
 
-    //TODO: get client's codeId from DB
+        //TODO: get client's codeId from DB
 
-    res.status(200);
-    res.json({'codeId': 'UnDeR CoNsTrUcTiOn'});
-  });
+        res.status(200);
+        res.json({ 'codeId': 'UnDeR CoNsTrUcTiOn' });
+    });
 
 router.route('/search')
-  .get(function(req, res){
-    if(!req.query.query){
-      res.status(400);
-      res.json({'error': 'No search query in request params'});
-    }
+    .get(function(req, res) {
+        if (!req.query.query) {
+            res.status(400);
+            res.json({ 'error': 'No search query in request params' });
+        }
 
-    if(!req.query.id){
-      res.status(400);
-      res.json({'error': 'No codeId in request params'});
-    }
+        if (!req.query.id) {
+            res.status(400);
+            res.json({ 'error': 'No codeId in request params' });
+        }
 
-    let searchQuery = req.query.query;
-    let id = req.query.id;
+        let searchQuery = req.query.query;
+        let id = req.query.id;
 
-    //TODO: Search for songs with spotify api
+        //TODO: Search for songs with spotify api
 
-    res.status(200);
-    res.json({results: []})
+        res.status(200);
+        res.json({ results: [] })
 
-  });
+    });
 
 router.route('/bid')
-  .post(function(req, res){
-    if(!req.body.cid){
-      res.status(400);
-      res.json({'error': 'No codeId in request params'});
-    }
+    .post(function(req, res) {
+        if (!req.body.cid) {
+            res.status(400);
+            res.json({ 'error': 'No codeId in request params' });
+        }
 
-    if(!req.body.sid){
-      res.status(400);
-      res.json({'error': 'No songId in request params'});
-    }
+        if (!req.body.sid) {
+            res.status(400);
+            res.json({ 'error': 'No songId in request params' });
+        }
 
-    if(!req.body.amount){
-      res.status(400);
-      res.json({'error': 'No amount in request params'});
-    }
+        if (!req.body.amount) {
+            res.status(400);
+            res.json({ 'error': 'No amount in request params' });
+        }
 
-    let cid = request.body.cid;
-    let sid = request.body.sid;
-    let amount = request.body.amount;
+        let cid = request.body.cid;
+        let sid = request.body.sid;
+        let amount = request.body.amount;
 
-    //TODO: Place bid in DB
+        //TODO: Place bid in DB
 
-    res.status(200)
-  });
+        res.status(200)
+    });
 
 
 router.route('/test')
-  .get(function(req, res){
-    res.status(418);
-    res.json({'ola': 'biba'});
+    .get(function(req, res) {
+        res.status(418);
+        res.json({ 'ola': 'biba' });
 
-});
+    });
 
 app.use('/', router);
 console.log("ready");
